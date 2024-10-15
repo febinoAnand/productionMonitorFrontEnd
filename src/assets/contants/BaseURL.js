20f65ae6103503c9c23b8cd7e18b2fc400021a32
anandkumar <febinosolutions@gmail.com>
2024-10-15 23:30:36 +0530

    BaseURL was added for development and production with .env

--- src/assets/contants/BaseURL.js
+++ src/assets/contants/BaseURL.js
@@ -4,10 +4,20 @@
 //     headers: {'X-Custom-Header': 'foobar'}
 //   });
 //   export default instance;
+// require('dotenv').config()
 
 // const BaseURL = "http://64.227.130.181:9002/"
 // const BaseURL = "http://django.febinosolutions.com/"
 // const BaseURL = "http://febinosolutions.com:9002/"
-const BaseURL = "https://productionb.univa.cloud/"
+// const BaseURL = "https://productionb.univa.cloud/"
 //const BaseURL = "http://localhost:9002/"
+
+let BaseURL = process.env.REACT_APP_LOCAL_BASE_URL ? process.env.REACT_APP_LOCAL_BASE_URL : "http://localhost:9020";
+if(process.env.REACT_APP_ENV === 'production'){
+    BaseURL = process.env.REACT_APP_REMOTE_BASE_URL
+}else if(process.env.REACT_APP_ENV === 'development'){
+    BaseURL = process.env.REACT_APP_DEVELOPMENT_BASE_URL
+}
+
+
 export default BaseURL
