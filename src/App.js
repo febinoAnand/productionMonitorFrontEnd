edd4164bbe866c404f055e721af5d5537edc907f
Dharikasri <shridharika@gmail.com>
2024-10-18 15:12:46 +0530

    added a producted route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Suspense } from 'react'
 import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './scss/style.scss'
+import { AuthProvider } from './AuthContext';
+import ProtectedRoute from './ProtectedRoute';
 
 
 const loading = (
@@ -25,6 +27,7 @@ const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 class App extends Component {
   render() {
     return (
+      <AuthProvider>
       <HashRouter>
         <Suspense fallback={loading}>
           <Routes>
@@ -35,12 +38,13 @@ class App extends Component {
             <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route exact path="/live" name="Home 2" element={<DashboardLayout />} />
-            <Route exact path="/machinelive" name="Machine 2" element={<MachineDashboardLayout />} />
-            <Route path="*" name="Home" element={<DefaultLayout />} />
+            <Route exact path="/live" name="Home 2" element={<ProtectedRoute element={DashboardLayout} />} />
+            <Route exact path="/machinelive" name="Machine 2" element={<ProtectedRoute element={MachineDashboardLayout} />} />
+            <Route path="*" name="Home" element={<ProtectedRoute element={DefaultLayout} />} />
           </Routes>
         </Suspense>
       </HashRouter>
+      </AuthProvider>
     )
   }
 }
